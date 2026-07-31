2e66d6d7952e335c0d592827e23bd43099a88305
Shehroz Riaz <zorhehs@gmail.com>
2026-07-31 11:29:07 +0500

    remove temp debug file

--- src/debug_doi.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-doi = "10.1371/journal.pone.0121283"
-url = f"https://api.unpaywall.org/v2/{doi}?email=test@example.com"
-response = requests.get(url)
-
-print("STATUS CODE:", response.status_code)
-print("RAW RESPONSE:", response.text[:500])
